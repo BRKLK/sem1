14f90be7f0b7af89a57b396c7c2c1ecca27f6fdf
BRKLK <burkalyek@gmail.com>
2025-11-03 19:48:04 +0200

    7 ready

--- 7.py
+++ 7.py
@@ -1,11 +1,27 @@
 def ShellSort(arr):
     l = len(arr)
-    gap = l//2
+    gap = l // 2
     swapped = False
 
-    while gap != 1 or swapped:
+    while gap > 1 or swapped:
+        swapped = False
+
         for i in range(gap, l):
-            if arr[i-gap] > arr[i]:
-                temp = arr[i]
-                arr[i] = arr[i-gap]
-                arr[i-gap] = temp
+            temp = arr[i]
+            j = i-gap
+            while j >= 0:
+                if arr[j] > temp:
+                    arr[j+gap] = arr[j]
+                    j -= gap
+                    swapped = True
+                else:
+                    break
+            arr[j + gap] = temp
+        gap = gap // 2
+
+    return arr
+
+
+arr1 = [4, 2, 6, 3, 5, 8, 12, 4, 22, 7]
+arr2 = [4, 2, 3, 1, 6]
+print(ShellSort(arr1))
